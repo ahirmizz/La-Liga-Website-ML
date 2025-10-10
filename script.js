c159abb99828b414ad50eb41816be1c5cdaa6c6b
ahirmizz <annabelleh421@gmail.com>
2025-10-10 01:33:11 -0700

    Add sidebar structure and base styling to homepage

--- script.js
+++ script.js
@@ -21,6 +21,40 @@ document.addEventListener("DOMContentLoaded", () => {
         console.warn("Dropdown button or container not found.");
     }
 
+    const toggleDropdown = (dropdown, menu, isOpen) => {
+    dropdown.classList.toggle("open", isOpen);
+    menu.style.height = isOpen ? `${menu.scrollHeight}px` : 0;
+}
+
+// Closes all open dropdowns when side bar is collapsed
+const closeAllDropdowns = () => {
+    document.querySelectorAll(".dropdown-container.open").forEach(openDropdown => {
+        toggleDropdown(openDropdown, openDropdown.querySelector(".dropdown-menu"), false);
+    })
+}
+
+document.querySelectorAll(".dropdown-toggle").forEach(dropdownToggle => {
+    dropdownToggle.addEventListener("click", e => {
+        e.preventDefault();
+
+        const dropdown = e.target.closest(".dropdown-container");
+        const menu = dropdown.querySelector(".dropdown-menu");
+        const isOpen = dropdown.classList.contains("open");
+
+        toggleDropdown(dropdown, menu, !isOpen);
+    });
+});
+
+
+document.querySelector(".sidebar-toggle").addEventListener("click", () => {
+    closeAllDropdowns();
+    
+    // Toggle collpased class on sidebar
+    document.querySelector(".sidebar").classList.toggle("collapsed");
+});
+
+
+
     if (!playerContainer) {
         console.error("Error - playerContainer not found.");
         return;
